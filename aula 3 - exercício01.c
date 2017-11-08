5406fa1fcb0dfcb41106ecf43e7f5664d1dd4260
marcosnonato <33500598+marcosnonato@users.noreply.github.com>
2017-11-08 20:03:14 -0300

    Create aula 3 - exercício01.c

---
+++ aula 3 - exercício01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* Dado um valor n, exiba uma contagem regressiva.*/
+
+int main(){
+
+	int i, n;
+	printf("\t\t Digite um número qualquer \n\n\t\t");
+	scanf("%i", &n);
+	
+	for( i = 0 ; i <= n; n--){
+		
+			printf("\n %d", n);
+		
+}
+
+return 0;
+
+}
